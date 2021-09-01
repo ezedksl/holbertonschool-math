279aceaef107f90488b3d9bbaebaf20db3134307
Eze <ezedksl@gmail.com>
2021-09-01 12:51:32 +0000

    update

--- 0-display.c
+++ 0-display.c
@@ -1,14 +1,19 @@
 #include "holberton.h"
 
+/**
+ * display_complex_number - Display complex number
+ * @c: 'complex' struct of numbers
+ **/
+
 void display_complex_number(complex c)
 {
-    if (c.im && c.re)
-        printf("%d + %di\n", c.re, c.im);
-    else
-    {
-        if (c.im)
-            printf("%di\n", c.im);
-        else
-            printf("%d\n", c.re);
-    }
+	if (c.im && c.re)
+		printf("%d + %di\n", (int)c.re, (int)c.im);
+	else
+	{
+		if (c.im)
+			printf("%di\n", (int)c.im);
+		else
+			printf("%d\n", (int)c.re);
+	}
 }
